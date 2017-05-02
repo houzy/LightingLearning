adb11d59b37ad328bedd050d764b29a5a228af62
houzy <houzy@houzydeMacBook-Pro-9.local>
2017-05-02 17:34:45 +0800

    Add LOD and Occlusion scenes

---
+++ Assets/Scripts/CubesManager.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * @Author: houzy 
+ * @Date: 2017-05-02 15:52:56 
+ * @Last Modified by: houzy
+ * @Last Modified time: 2017-05-02 16:06:59
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 创建大量Cube供测试
+/// </summary>
+public class CubesManager : MonoBehaviour {
+
+	public int CubeNum = 5;
+	public float CubeSize = 0.1f;
+	public float Interval = 0.2f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
